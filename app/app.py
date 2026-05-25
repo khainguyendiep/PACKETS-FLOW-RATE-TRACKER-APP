323851eb0b4ba3db091c4d2506cd9c1466d69a00
khainguyen <khainguyendiep1107@gmail.com>
2026-05-26 00:03:18 +0700

    [feat]: Added interactive features for the graph and changed displayed time

--- app/app.py
+++ app/app.py
@@ -3,22 +3,26 @@ import pandas as pd
 import altair as alt
 import numpy as np
 
-df_chart = pd.read_csv("/var/log/packets_flow_rate_tracker/packets_flow_rate_tracker.log", header = None, names=["packets","time"])
+df_chart = pd.read_csv("/var/log/packets_flow_rate_tracker/packets_flow_rate_tracker.log", header = None, names=["packets","times"])
+
+df_chart['times'] = pd.to_datetime(df_chart['times'], format='%H:%M:%S')
 
 df_chart = alt.Chart(df_chart).mark_line().encode(
     x=alt.X(
-        'time', 
+        'times:T', 
         # 1. Increase the visible range boundary
         # scale=alt.Scale(domain=[0, 25]),
         axis=alt.Axis(
+            format='%H:%M:%S',
             labelAngle=-45,       # Slants the text at an oblique 45-degree angle
             labelAlign='right',   # Aligns the top-right of the text to the tick mark
             labelBaseline='middle', # Keeps the text vertically centered relative to the angle
             tickCount=10
         )
     ),
-    y='packets'
-)
+    y='packets:Q'
+).interactive()
+
 st.title("FLOW RATE TRACKER")
 tab1, = st.tabs(["Chart"])
 with tab1:
