232e7dbb013673424875bdef073a0d9792d5fa4e
khainguyen <khainguyendiep1107@gmail.com>
2026-05-26 00:01:09 +0700

    [feat]: change exported time format

--- core/logger/logger.cpp
+++ core/logger/logger.cpp
@@ -10,10 +10,10 @@ void write_csv(long number_of_packet, const std::string &LOG_PATH){
     std::time_t now_c = std::chrono::system_clock::to_time_t(local_time_now);
     std::tm* ltm = std::localtime(&now_c);
 	std::ostringstream timestamp;
-	timestamp << std::put_time(ltm, "%H %M %S");
+	timestamp << std::put_time(ltm, "%H:%M:%S");
 	if (log_file.is_open()) {
         // dump() with no arguments ensures the JSON is on one single line
-        log_file << number_of_packet <<", " << timestamp.str() << std::endl;
+        log_file << number_of_packet <<"," << timestamp.str() << std::endl;
         log_file.close();
     }
 }
